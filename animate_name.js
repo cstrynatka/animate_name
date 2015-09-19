c3b90fd12ec077a5e4af57cf1fbd41a704a8a27b
Christina <christina@ChristiasMacAir.lan>
2015-09-18 21:32:55 -0400

    Created a name animation file using JavaScript and HTML

---
+++ animate_name.js
@@ -0,0 +1,20 @@
+var myName = "Christina Strynatka";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var yellow = [75, 100, 40];
+var green = [196, 77, 55]
+var blue = [280, 50, 60];
+var purple = [340, 40, 60];
+
+var letterColours = [red, orange, yellow, green, blue, purple];
+
+drawName(myName, letterColours);
+
+if( 10 < 3 ) {
+    bubbleShape = "square";
+}else{
+    bubbleShape = "circle;
+}
+
+bounceBubbles();
